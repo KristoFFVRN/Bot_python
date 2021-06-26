8ac174cd7abe2326e0e8728fecbca7e11cd52cba
KristOFFVRN <dakryukov@rt-dc.ru>
2021-06-26 16:40:30 +0300

    last commit

--- Бот_1.py
+++ Бот_1.py
@@ -9,8 +9,9 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 )
 def start_bot(update: Updater, context: CallbackContext):
     print(update)
-    mytext = """Привет dfkthf {}
+    mytext = """Привет {}
     я могу использовать только команду  /start =)""".format(update.message.chat.first_name)
+    logging.info('User{} press /start'.format(update.message.chat.username))
     update.message.reply_text(mytext)
 
 def chat(update: Updater, context: CallbackContext):
