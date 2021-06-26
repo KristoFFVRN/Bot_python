bc7cde5fd6e94eaff78ff09f9abd6d10dea6f218
KristOFFVRN <dakryukov@rt-dc.ru>
2021-06-26 15:36:02 +0300

    innitian commit

---
+++ Бот_1.py
@@ -0,0 +1,32 @@
+import logging
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+)
+def start_bot(update: Updater, context: CallbackContext):
+    print(update)
+    mytext = """Привет {}
+    я могу использовать только команду  /start =)""".format(update.message.chat.first_name)
+    update.message.reply_text(mytext)
+
+def chat(update: Updater, context: CallbackContext):
+    text= update.message.text
+    logging.info(text)
+
+    update.message.reply_text(text)
+
+
+def main():
+    updtr = Updater('1449256444:AAEXKP965duuF7ufMEukRAH_8nBjSjt_k9E')
+
+    updtr.dispatcher.add_handler(CommandHandler("start",start_bot))
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text,chat))
+
+    updtr.start_polling()
+    updtr.idle()
+if __name__ == "__main__":
+    logging.info('Bot started!')
+    main()
