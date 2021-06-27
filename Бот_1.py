a077d8a3b9f3dc6bd3aec60a73fc25887eeb609b
KristOFFVRN <dakryukov@rt-dc.ru>
2021-06-27 23:36:33 +0300

    beward_rebut commit

--- Бот_1.py
+++ Бот_1.py
@@ -1,34 +1,36 @@
 import logging
 import settings
+import telebot
+import webbrowser
+import requests
+import time
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from telebot import types
 
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log'
-)
-def start_bot(update: Updater, context: CallbackContext):
-    
-    mytext = """Привет {}
-    я могу использовать только команду  /start =)""".format(update.message.chat.first_name)
-    logging.info('User{} press /start'.format(update.message.chat.username))
-    update.message.reply_text(mytext)
-
-def chat(update: Updater, context: CallbackContext):
-    text= update.message.text
-    logging.info(text)
-
-    update.message.reply_text(text)
+ip_adrr = ''
+URL = ''
+bot = telebot.TeleBot(settings.TOKEN)
 
 
-def main():
-    updtr = Updater(settings.TOKEN_TELEGRAM)
+@bot.message_handler(content_types=['text'])
+def handle_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет")
+    elif message.text == '/rebot':
+        bot.send_message(message.from_user.id, "Ввиди ip-адрес!")
+        bot.register_next_step_handler(message, ip_beward)
 
-    updtr.dispatcher.add_handler(CommandHandler("start",start_bot))
-    updtr.dispatcher.add_handler(MessageHandler(Filters.text,chat))
 
-    updtr.start_polling()
-    updtr.idle()
-if __name__ == "__main__":
-    logging.info('Bot started!')
-    main()
+def ip_beward(message):
+    global ip_adrr
+    global URL
+    ip_adrr = message.text
+    URL = "http://" + ip_adrr + "/cgi-bin/restart_cgi?&user=admin&pwd=admin"
+    webbrowser.register('Chrome', None, webbrowser.BackgroundBrowser(
+        'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'))
+    webbrowser.open_new(URL)
+    print(URL)
+
+
+bot.polling()
