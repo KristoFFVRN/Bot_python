d444dabb2f5ec15c5f9b3a04c5b5112c1230b6b1
KristOFFVRN <dakryukov@rt-dc.ru>
2021-06-28 02:13:06 +0300

    2.0 commit

--- Бот_1.py
+++ Бот_1.py
@@ -14,6 +14,7 @@ ip_adr = ''
 URL = ''
 URL1 = ''
 Nomer_kvartiri = ''
+nomer = 0
 bot = telebot.TeleBot(settings.TOKEN)
 
 
@@ -40,22 +41,31 @@ def reboot_beward(message):
 
 def zvonok_beward(message):
     global ip_adr
-    
     ip_adr = message.text
-    bot.send_message(message.from_user.id, "Ввиди номер квартиры")
+    bot.send_message(message.from_user.id, "Ввидите номер квартиры!")
+    bot.register_next_step_handler(message, zvonok2_beward)
+
+def zvonok2_beward(message):
+    global nomer
+    if message.text != 0:
+        nomer= message.text
+        print(nomer)
+        URL_GET = "http://" + ip_adr + "/cgi-bin/apartment_cgi?action=get&Number="+nomer+ "&user=admin&pwd=admin"
+        webbrowser.register('Chrome', None, webbrowser.BackgroundBrowser('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'))
+        webbrowser.open_new(URL_GET)
+    bot.send_message(message.from_user.id, "Ввидите номер дозвона!")
     bot.register_next_step_handler(message, zvonok1_beward) 
     
-    URL_GET = "http://" + ip_adr + "/cgi-bin/apartment_cgi?action=get&Number=3"
-    
-    print(URL_GET)
 def zvonok1_beward(message):
     global URL1
     global Nomer_kvartiri
     Nomer_kvartiri = message.text
-    
     URL1 = "http://" + ip_adr + "/cgi-bin/sip_cgi?action=call&Uri=" +Nomer_kvartiri + "&user=admin&pwd=admin"
     webbrowser.register('Chrome', None, webbrowser.BackgroundBrowser('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'))
     webbrowser.open_new(URL1)
-    print(URL1)
+    bot.send_message(message.from_user.id, "Вызов отправлен!")
 
+
+
+    
 bot.polling()
